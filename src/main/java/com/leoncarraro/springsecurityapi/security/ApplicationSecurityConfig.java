00abbede0b4b7d35b2ed3efce975b8ccb375234c
Leonardo Carraro <lcarraro.oliveira@gmail.com>
2020-09-22 11:19:26 -0300

    Create a new in memory user and encrypt yours password

--- src/main/java/com/leoncarraro/springsecurityapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/leoncarraro/springsecurityapi/security/ApplicationSecurityConfig.java
@@ -1,14 +1,24 @@
 package com.leoncarraro.springsecurityapi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -18,6 +28,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authenticated()
 			.and()
 			.httpBasic();
+	}
+	
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails annaSmithUser = User.builder()
+			.username("annasmith")
+			.password(passwordEncoder.encode("password"))
+			.roles("STUDENT")
+			.build();
+		
+		return new InMemoryUserDetailsManager(annaSmithUser);
 	}
 	
 }
