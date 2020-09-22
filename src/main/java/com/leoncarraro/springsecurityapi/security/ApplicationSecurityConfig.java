2d32cdf78533655cf240af9d9c53d0d989ee218f
Leonardo Carraro <lcarraro.oliveira@gmail.com>
2020-09-22 10:16:49 -0300

    Add URLs to whitelist

--- src/main/java/com/leoncarraro/springsecurityapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/leoncarraro/springsecurityapi/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/", "index", "/css/*", "/js/*")
+			.permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
