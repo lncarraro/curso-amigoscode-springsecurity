82c41f2196ff5e172d62b27ee30833bfca027cd9
Leonardo Carraro <lcarraro.oliveira@gmail.com>
2020-09-23 10:20:05 -0300

    Remove comments and set form fields parameters name

--- src/main/java/com/leoncarraro/springsecurityapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/leoncarraro/springsecurityapi/security/ApplicationSecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,29 +27,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-//			.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//			.and()
-			.csrf().disable()
+		http.csrf().disable()
 			.authorizeRequests()
-			.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-			.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
-//			.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.STUDENT_WRITE.getPermission())
-//			.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.STUDENT_WRITE.getPermission())
-//			.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.STUDENT_WRITE.getPermission())
-//			.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name())
-			.anyRequest()
-			.authenticated()
+				.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+				.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+				.anyRequest().authenticated()
 			.and()
 			.formLogin()
-			.loginPage("/login").permitAll()
-			.defaultSuccessUrl("/courses", true)
+				.loginPage("/login").permitAll()
+				.defaultSuccessUrl("/courses", true)
+				.usernameParameter("username")
+				.passwordParameter("password")
 			.and()
 			.rememberMe()
 				.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+				.rememberMeParameter("remember-me")
 			.and()
 			.logout()
 				.logoutUrl("/logout")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name()))  // Only when CSRF are disabled!! Otherwise, http method should be a POST request
 				.clearAuthentication(true)
 				.invalidateHttpSession(true)
 				.deleteCookies("JSESSIONID", "remember-me")
@@ -60,21 +58,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		UserDetails annaSmithUser = User.builder()
 			.username("annasmith")
 			.password(passwordEncoder.encode("password"))
-//			.roles(ApplicationUserRole.STUDENT.name())
 			.authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
 			.build();
 		
 		UserDetails lindaUser = User.builder()
 			.username("linda")
 			.password(passwordEncoder.encode("admin"))
-//			.roles(ApplicationUserRole.ADMIN.name())
 			.authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
 			.build();
 		
 		UserDetails tomUser = User.builder()
 			.username("tom")
 			.password(passwordEncoder.encode("admintrainee"))
-//			.roles(ApplicationUserRole.ADMIN_TRAINEE.name())
 			.authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
 			.build();
 		
